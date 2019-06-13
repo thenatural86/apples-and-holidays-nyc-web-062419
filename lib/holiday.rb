3ce7c8da85791f6200725a89d6b3bcd249b9cb1a
The Natural <philip.smalls86@gmail.com>
2019-06-13 02:38:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
+  binding.pry
 end 
 
 def all_winter_holiday_supplies(holiday_hash)
